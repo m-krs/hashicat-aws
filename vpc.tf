c8d62bc04a5df120d24ea545a029dfb2c53ee2fa
Your Name <you@example.com>
2020-09-22 16:14:21 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -2,4 +2,10 @@ module "vpc" {
   source  = "app.terraform.io/mike-sky-training/vpc/aws"
   version = "2.52.0"
   # insert required variables here
+  vpc_id     = aws_vpc.hashicat.id
+  cidr_block = var.subnet_prefix
+
+  tags = {
+    name = "${var.prefix}-subnet"
+  }
 }
